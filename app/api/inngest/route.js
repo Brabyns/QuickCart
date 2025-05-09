a037e11c2cf71cee4efdeb02cb05f9a1b14b91f1
Brabyns Yabwetsa <brabyns.yabwetsa.dev@gmail.com>
2025-05-09 07:03:44 +0300

    fix: corrected inngest import and path issues

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
-// import { serve } from "inngest/next";
-import { serve } from "inngest/next/app-router";
-import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion} from "@/config/inngest";
+import { serve } from "inngest/next";
+// import { serve } from "inngest/next/app-router";
+import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion} from "../../../config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
